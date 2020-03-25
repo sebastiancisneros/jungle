e0ae6a80d34a434342c73f616eba610453c35645
Sebastian Cisneros <sebastiancisneros@gmail.com>
2020-03-25 12:47:56 -0700

    list of requirements

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php echo 'Por Fin' ?>
+<?php
+  echo '<HEADER>Jungle Order Shipping</HEADER>';
+  echo 'Load Order Lists';
+  echo '<br/>';
+  echo 'Expand Individual Order'
+  echo '<br/>';
+  echo 'Enter Package Weight'
+  echo '<br/>';
+  echo 'Select Shipping Carrier'
+  echo '<br/>';
+  echo 'Purchase Shipping Label'
+  echo '<br/>';
+  echo 'Print Label'
+  echo '<br/>';
+  echo 'Send Shipping Confirmation'
+?>
