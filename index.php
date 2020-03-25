630482ee4f096c0d4863c63671e61c1ff74d7a3a
Sebastian Cisneros <sebastiancisneros@gmail.com>
2020-03-25 12:50:31 -0700

    changes

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
   echo '<HEADER>Jungle Order Shipping</HEADER>';
   echo 'Load Order Lists';
   echo '<br/>';
-  echo 'Expand Individual Order'
+  echo 'Expand Individual Order';
   echo '<br/>';
-  echo 'Enter Package Weight'
+  echo 'Enter Package Weight';
   echo '<br/>';
-  echo 'Select Shipping Carrier'
+  echo 'Select Shipping Carrier';
   echo '<br/>';
-  echo 'Purchase Shipping Label'
+  echo 'Purchase Shipping Label';
   echo '<br/>';
-  echo 'Print Label'
+  echo 'Print Label';
   echo '<br/>';
-  echo 'Send Shipping Confirmation'
+  echo 'Send Shipping Confirmation';
 ?>
