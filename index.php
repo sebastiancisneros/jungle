8a08d5d53a5095f1688e57d2374a404c4f03b3ab
Sebastian Cisneros <sebastiancisneros@gmail.com>
2020-04-01 14:45:41 -0700

    added php errors

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
 date_default_timezone_set('America/Los_Angeles');
 
 require_once("shippingRates.php");
