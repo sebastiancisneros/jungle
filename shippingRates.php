8bdbf8e68d6e8d2a10f6c495e63c956f321ec0ca
Sebastian Cisneros <sebastiancisneros@gmail.com>
2020-04-01 16:24:46 -0700

    report errors shipping

--- shippingRates.php
+++ shippingRates.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 
 require_once("easypost-php/lib/easypost.php");
